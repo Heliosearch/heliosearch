be9d7dff52cfc94b1aad06114ba5faef29d10504
Joel Bernstein <joel@new-host.home>
2014-10-27 09:33:26 -0400

    Removed debugging

--- solr/solrj/src/java/org/apache/solr/client/solrj/streaming/HashKey.java
+++ solr/solrj/src/java/org/apache/solr/client/solrj/streaming/HashKey.java
@@ -33,7 +33,6 @@ public class HashKey implements Serializable {
   public HashKey(Tuple t, String[] keys) {
     this.parts = new Object[keys.length];
     for(int i=0; i<keys.length; i++) {
-      System.out.println("#################################################:"+keys[i]+":"+t.get(keys[i]));
       parts[i] = t.get(keys[i]);
     }
   }
